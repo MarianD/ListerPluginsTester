d878acd99d014603ca0e88cb501e2cbd139bde14
Marian Denes <mardenes@gmail.com>
2013-05-15 15:55:00 +0200

    Project settings and source changes for 64-bit version, too

--- Constants.h
+++ Constants.h
@@ -1,8 +1,8 @@
 #ifndef CONSTANTS_H_INCLUDED
     #define CONSTANTS_H_INCLUDED
 
-    const char * const szClassName = "ListerPluginsTester";
-    const char * const menoDLL     = "CharsOccurrences.wlx";
+    const char * const szClassName = "ListerPluginsTester";     // "ListerPluginsTester 64-bit" for 64-bit version
+    const char * const menoDLL     = "CharsOccurrences.wlx";    // "CharsOccurrences.wlx64" for 64-bit version
     const char * const menoSuboru  = "C:\\LoremIpsum.txt";
 
 #endif // CONSTANTS_H_INCLUDED
